42ca2590a87e2097e9381fbcbd010e338d219729
Steven Than <steventhan11@gmail.com>
2018-02-28 01:39:38 -0500

    added all the pages

---
+++ src/components/Membership.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+class Membership extends Component {
+  render() {
+    return (
+      <div>Membership</div>
+    )
+  }
+}
+
+export default Membership;
---
+++ src/components/MyReservations.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+
+class MyReservations extends Component {
+  render() {
+    return (
+      <div>My reservations</div>
+    )
+  }
+}
+
+export default MyReservations;
