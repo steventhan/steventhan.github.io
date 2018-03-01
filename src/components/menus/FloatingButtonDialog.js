fc7ca1e38bbc82f895cd83e658c5dea9472b0187
Steven Than <steventhan11@gmail.com>
2018-03-01 04:38:11 -0500

    dialog for FloatingButton

---
+++ src/components/menus/FloatingButtonDialog.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-unused-vars*/
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import { Divider, InputAdornment, Input, InputLabel, GridList, GridListTile, GridListTileBar,
+  Button, ButtonBase, Typography, Grid, Radio, TextField, FormControlLabel } from 'material-ui';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  withMobileDialog,
+} from 'material-ui/Dialog';
+
+import bikeImg from "../../bike-real.jpg";
+import ellipticalImg from "../../elliptical-real.jpg";
+import stepmillImg from "../../stepmill-real.jpg";
+import treadmillImg from "../../treadmill-real.jpg";
+import pullupImg from "../../pullup-real.png";
+
+const machineTypes = [
+  {name: "Bike", img: bikeImg},
+  {name: "Elliptical", img: ellipticalImg},
+  {name: "Pull-up", img: pullupImg},
+  {name: "Stepmill", img: stepmillImg},
+  {name: "Treadmill", img: treadmillImg},
+];
+
+
+class FloatingButtonDialog extends Component {
+  state = {
+    open: false,
+  };
+
+  render() {
+    const { fullScreen } = this.props;
+
+    return (
+      <Dialog
+        fullScreen={fullScreen}
+        open={this.props.open}
+        onClose={this.props.handleDialogClose}
+        aria-labelledby="responsive-dialog-title"
+      >
+        <DialogTitle id="responsive-dialog-title">
+          <strong>Choose machine type</strong>
+        </DialogTitle>
+        <DialogContent>
+          <GridList cellHeight={150}>
+            {machineTypes.map((type, i) => (
+              <GridListTile
+                key={i}
+                component={Link}
+                to="/reserve"
+                onClick={this.props.handleDialogClose}>
+                  <img src={type.img} alt={type.name} />
+                  <GridListTileBar
+                    title={type.name}
+                  />
+                  {type.name}
+              </GridListTile>
+            ))}
+          </GridList>
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.props.handleDialogClose} variant="raised" color="default">
+            Discard
+          </Button>
+          <Button
+            component={Link}
+            to="/reserve"
+            onClick={this.props.handleDialogClose}
+            variant="raised"
+            color="primary">
+            View all
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+export default withMobileDialog()(FloatingButtonDialog);
