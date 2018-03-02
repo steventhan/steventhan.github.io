35585372041b7741aff209fc8c4658ee43fc644b
Steven Than <steventhan11@gmail.com>
2018-03-02 02:53:20 -0500

    added sth to Membership

--- src/components/Membership.js
+++ src/components/Membership.js
@@ -1,12 +1,15 @@
-/* eslint-disable no-unused-vars*/
 import React, { Component } from "react";
-import { Grid } from "material-ui";
+import { Grid, Typography } from "material-ui";
 
 
 class Membership extends Component {
   render() {
     return (
-      <Grid>Membership</Grid>
+      <Grid container style={{marginTop: 75}} justify="center" alignItems="center" spacing={0}>
+        <Grid item>
+          <Typography>This page will contain membership information</Typography>
+        </Grid>
+      </Grid>
     )
   }
 }
